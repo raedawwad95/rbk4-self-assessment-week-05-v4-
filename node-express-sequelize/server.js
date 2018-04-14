26746f29908497d27e69690bc0e818cf508c43b3
“raedawwad95” <awwad12121@gmail.com>
2018-04-14 09:56:21 +0300

    solved

--- node-express-sequelize/server.js
+++ node-express-sequelize/server.js
@@ -9,7 +9,11 @@ var User = sequelize.define('User', {
 
 /*  Create a '/users' route that responds to 
     a GET request with all users in the database */
-
+app.get('/users',function(req,res){
+	User.findAll(attributes:['username']).then(users =>{
+		res.send(JSON.parse(users))
+	})
+})
 
 
 module.exports = { 
